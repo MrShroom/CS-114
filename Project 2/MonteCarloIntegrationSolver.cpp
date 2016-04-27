163667692abb184e9208c336492a6a813510577d
Shaun McThomas <smcthoma@uci.edu>
2016-04-27 03:09:38 -0700

    Part 1 tasks 1 and 2 done.

--- Project 2/MonteCarloIntegrationSolver.cpp
+++ Project 2/MonteCarloIntegrationSolver.cpp
@@ -26,17 +26,52 @@ SolverResults_t* MonteCarloIntegrationSolver::solve()
 			break;
 		}
 
-		results->I[i] = m_funtion_slove(x) / m_funtion_density(x);
+		results->I[i] = m_funtion_solve(x) / m_funtion_density(x);
 		sum_of_I += results->I[i];
 	}
 	results->I_bar = sum_of_I / NUMBER_OF_SAMPLES;
 	return results;
 }
 
-
-void MonteCarloIntegrationSolver::setFuntion(function_ptr funtion_slove)
+SolverResults_t * MonteCarloIntegrationSolver::solveArr(int arraySize)
 {
-	m_funtion_slove = funtion_slove;
+    SolverResults_t* results = new SolverResults_t();
+    double *x = new double[arraySize];
+    double sum_of_I = 0.0;
+
+    for (int i = 0; i < NUMBER_OF_SAMPLES; i++)
+    {
+        for (int j = 0; j < arraySize; j++)
+        {
+            switch (m_curreunt_distribution)
+            {
+            case UNIFORM:
+                x[j] = m_uniform_distribution[j](*m_generator);
+                break;
+            case EXPONENTENIAL:
+                x[j] = m_exponential_distribution[j](*m_generator);
+                break;
+            default:
+                x[j] = 0;
+                break;
+            }
+        }
+        results->I[i] = m_funtion_solve_arr(x, arraySize) / m_funtion_density_arr(x, arraySize);
+        sum_of_I += results->I[i];
+    }
+    results->I_bar = sum_of_I / NUMBER_OF_SAMPLES;
+    return results;
+}
+
+
+void MonteCarloIntegrationSolver::setFuntion(function_ptr funtion_solve)
+{
+	m_funtion_solve = funtion_solve;
+}
+
+void MonteCarloIntegrationSolver::setFuntion(function_ptr_array function_solve_array)
+{
+    m_funtion_solve_arr = function_solve_array; 
 }
 
 void MonteCarloIntegrationSolver::setDensityFunctionAsExp(function_ptr funtion_density, double lambda)
@@ -59,6 +94,36 @@ void MonteCarloIntegrationSolver::setDensityFunctionAsUni(function_ptr funtion_d
 		delete(m_uniform_distribution);
 	}
 	m_uniform_distribution = new std::uniform_real_distribution<double>(a, b);
+}
+
+void MonteCarloIntegrationSolver::setDensityFunctionAsExp(function_ptr_array funtion_density, double lambda, int array_size)
+{
+    m_funtion_density_arr = funtion_density;
+    m_curreunt_distribution = EXPONENTENIAL;
+    if (m_exponential_distribution != 0)
+    {
+        delete(m_exponential_distribution);
+    }
+    m_exponential_distribution = new std::exponential_distribution<double>[array_size];
+    for (int i = 0; i < array_size; i++)
+    {
+        m_exponential_distribution[i] = std::exponential_distribution<double>(lambda);
+    }
+}
+
+void MonteCarloIntegrationSolver::setDensityFunctionAsUni(function_ptr_array funtion_density, double a[], double b[] ,int array_size)
+{
+    m_funtion_density_arr = funtion_density;
+    m_curreunt_distribution = UNIFORM;
+    if (m_uniform_distribution != 0)
+    {
+        delete(m_uniform_distribution);
+    }
+    m_uniform_distribution = new std::uniform_real_distribution<double>[array_size];
+    for (int i = 0; i < array_size; i++)
+    {
+        m_uniform_distribution[i] = std::uniform_real_distribution<double>(a[i], b[i]);
+    }
 }
 
 MonteCarloIntegrationSolver::~MonteCarloIntegrationSolver()
--- Project 2/MonteCarloIntegrationSolver.h
+++ Project 2/MonteCarloIntegrationSolver.h
@@ -1,9 +1,10 @@
 #pragma once
-#include<random>
+#include <random>
 
 #define NUMBER_OF_SAMPLES 100000
 
 typedef double(*function_ptr)(double);
+typedef double(*function_ptr_array)(double* , int );
 
 enum Distro
 {
@@ -20,13 +21,24 @@ class MonteCarloIntegrationSolver {
 public:
 	MonteCarloIntegrationSolver();
 	SolverResults_t* solve();
+    SolverResults_t* solveArr(int arraySize);
+
 	void setFuntion(function_ptr funtion_slove);
+    void setFuntion(function_ptr_array function_solve_array);
+
 	void setDensityFunctionAsExp(function_ptr funtion_density, double lambda);
 	void setDensityFunctionAsUni(function_ptr funtion_density, double a, double b);
+
+    void setDensityFunctionAsExp(function_ptr_array funtion_density, double lambda, int arraySize);
+    void setDensityFunctionAsUni(function_ptr_array funtion_density, double a[], double b[], int array_size);
+
 	~MonteCarloIntegrationSolver();
 private:
-	function_ptr m_funtion_slove;
+	function_ptr m_funtion_solve;
+    function_ptr_array m_funtion_solve_arr;
 	function_ptr m_funtion_density;
+    function_ptr_array m_funtion_density_arr;
+
 	std::random_device rd;
 	std::mt19937 *m_generator = new std::mt19937(rd());
 	std::uniform_real_distribution<double> *m_uniform_distribution = 0;
